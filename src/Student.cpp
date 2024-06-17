2e2dedc678de00746b8d100ba8e38a54f409e40d
tiendatpQuest <tiendatp@questps.com.au>
2024-06-17 17:30:59 +1000

    Student class

---
+++ src/Student.cpp
@@ -0,0 +1,69 @@
+#include "Student.h"
+#include <stdexcept>
+// Constructor:
+
+Student::Student(const std::string &studentName, const std::string &studentID, int currentYear, int currentSemester) : itsStudentName(studentName), itsStudentID(studentID), itsCurrentYear(currentYear), itsCurrentSemester(currentSemester)
+{
+    int totalSemesters = (currentYear - 1) * 2 + (currentSemester - 1);
+    semesters.resize(totalSemesters);
+}
+
+std::string Student::GetStudentName() const { return itsStudentName; }
+std::string Student::GetStudentID() const { return itsStudentID; }
+int Student::GetCurrentYear() const { return itsCurrentYear; }
+int Student::GetCurrentSemester() const { return itsCurrentSemester; }
+void Student::addCourse(int semesterIndex, const Course &course)
+{
+    if (semesterIndex >= 0 && semesterIndex < semesters.size())
+    {
+        semesters[semesterIndex].push_back(course);
+    }
+}
+
+double Student::calculateGPA(int semesterIndex) const
+{
+    if (semesterIndex >= 0 && semesterIndex < semesters.size())
+    {
+        double totalPoints = 0;
+        int totalUnits = 0;
+        for (const auto &course : semesters[semesterIndex])
+        {
+            int gpaPoint = 0;
+            if (course.GetItsGrades() > 80)
+                gpaPoint = 4;
+            else if (course.GetItsGrades() >= 70)
+                gpaPoint = 3;
+            else if (course.GetItsGrades() >= 60)
+                gpaPoint = 2;
+            else if (course.GetItsGrades() >= 50)
+                gpaPoint = 1;
+            // Fail grade does not count
+            if (course.GetItsGrades())
+            {
+                totalUnits++;
+                totalPoints += gpaPoint;
+            }
+        }
+        return (totalUnits > 0) ? (totalPoints / totalUnits) : 0;
+    }
+    return 0;
+}
+
+double Student::calculateCGPA() const
+{
+    int totalSemester = semesters.size();
+    int totalGPA = 0;
+    for (int i = 0; i < semesters.size(); i++)
+    {
+        try
+        {
+            totalGPA += calculateGPA(i);
+        }
+        catch (const std::exception &e)
+        {
+            // Handle error
+            throw;
+        }
+    }
+    return (totalSemester > 0) ? (totalGPA / totalSemester) : throw std::runtime_error("No semesters available");
+}
---
+++ test/TestStudent.cpp
@@ -0,0 +1,74 @@
+#include "../src/Student.h"
+#include "../src/Course.h"
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+// Test Student Constructor:
+
+void TestStudentConstructor()
+{
+    Student student1("Pham Tien Dat", "103174539", 4, 1);
+    assert(student1.GetStudentName() == "Pham Tien Dat");
+    assert(student1.GetStudentID() == "103174539");
+    assert(student1.GetCurrentYear() == 4);
+    assert(student1.GetCurrentSemester() == 1);
+    std::cout << "Test constructor passed" << std::endl;
+}
+
+// test student getter method:
+void TestStudentGetterMethod()
+{
+    Student student2("Ho Vu Phuong Anh", "103171611", 4, 2);
+    assert(student2.GetStudentName() == "Ho Vu Phuong Anh");
+    assert(student2.GetStudentID() == "103171611");
+    assert(student2.GetCurrentYear() == 4);
+    assert(student2.GetCurrentSemester() == 2);
+    std::cout << "Test Student Getter Method Passed" << std::endl;
+}
+
+// test add course method:
+void TestAddCourseMethod()
+{
+    Student student3("Nguyen Dang Khanh Toan", "103123571", 3, 2);
+    Course course1("Introduction to Programming", 95);
+    student3.addCourse(0, course1);
+    assert(student3.calculateGPA(0) == 4);
+    std::cout << "Test add course passed" << std::endl;
+}
+
+void TestCalculateGPA()
+{
+    Student student4("Dat Pham", "103174539", 4, 1);
+    Course course1("Object Oriented Programming", 90);
+    Course course2("User Centred Design", 65);
+    student4.addCourse(0, course1);
+    student4.addCourse(0, course2);
+    assert(student4.calculateGPA(0) == 3);
+    std::cout << "Test calculate GPA passed" << std::endl;
+}
+
+void TestCalculateCGPA()
+{
+    Student student5("Dep trai va dang cap", "1250605606", 5, 1);
+    Course course1("Calculus ", 100);
+    Course course2("Linear Algebra", 99);
+    Course course3("Electronic and electromagnetism", 67);
+    student5.addCourse(0, course1);
+    student5.addCourse(1, course2);
+    student5.addCourse(2, course3);
+    double cGPA = student5.calculateCGPA();
+    assert(cGPA > 0); // simple check to ensure CGPA is calculated
+    std::cout << "Test calculate CGPA passed" << std::endl;
+}
+
+int main()
+{
+    TestStudentConstructor();
+    TestStudentGetterMethod();
+    TestAddCourseMethod();
+    TestCalculateGPA();
+    TestCalculateCGPA();
+    std::cout << "All tests passed" << std::endl;
+    return 0;
+}
